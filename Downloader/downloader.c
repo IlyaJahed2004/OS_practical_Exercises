98a07be5333e634d7b9f994ba15edb35ef010d7b
Ilya Jahed <ilyajahed.w2004@gmail.com>
2025-11-13 19:40:23 +0330

    Implement get_file_size() using libcurl HEAD request with error handling

--- Downloader/downloader.c
+++ Downloader/downloader.c
@@ -14,6 +14,7 @@ struct ThreadData {
     int part_no;        // Thread number (part index)
 };
 
+
 // Function prototypes (declarations)
 // Each function will be implemented in later stages
 void *download_part(void *arg);                  // Download a specific part of the file
@@ -23,4 +24,40 @@ void merge_files(const char *filename, int num_parts);  // Combine downloaded pa
 int main(int argc, char *argv[]) {
     // Entry point of the program
     // Will handle argument parsing, thread creation, and file merging
+}
+
+
+
+// Function to get the size of a file (in bytes) using a HEAD request
+long get_file_size(const char *url) {
+    CURL *curl = curl_easy_init();    // Create a CURL handle
+    double filesize = 0.0;            // libcurl stores size as a double
+    CURLcode res;
+
+    if (curl) {
+        // Set the target URL
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+
+        // Make a HEAD request (no body)
+        curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
+
+        // Include headers in the response (optional)
+        curl_easy_setopt(curl, CURLOPT_HEADER, 1L);
+
+        // Perform the request
+        res = curl_easy_perform(curl);
+
+        // If successful, retrieve the Content-Length
+        if (res == CURLE_OK) {
+            curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &filesize);
+        } else {
+            fprintf(stderr, "Error: %s\n", curl_easy_strerror(res));
+            filesize = -1;  // indicate failure
+        }
+
+        // Clean up and free resources
+        curl_easy_cleanup(curl);
+    }
+
+    return (long)filesize; // Return file size (or -1 if failed)
 }
