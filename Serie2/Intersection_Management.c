2d4a85e7425c50054953db184457de36cebce77a
Ilya Jahed <ilyajahed.w2004@gmail.com>
2025-12-03 15:35:46 +0330

    second version:Handling deadlocks

--- Serie2/Intersection_Management.c
+++ Serie2/Intersection_Management.c
@@ -6,7 +6,7 @@
 #include <unistd.h>
 #include <time.h>
 
-#define CAR_COUNT_PER_SPAWNER 20
+#define CAR_COUNT_PER_SPAWNER 3
 #define MAX_SLEEP 3
 
 enum Direction{
@@ -16,18 +16,42 @@ enum Direction{
     WEST
 };
 
+const char* dir_to_string(int dir){
+    switch(dir){
+        case NORTH: return "NORTH";
+        case SOUTH: return "SOUTH";
+        case EAST:  return "EAST";
+        case WEST:  return "WEST";
+        default:    return "UNKNOWN";
+    }
+}
+
 pthread_mutex_t sections[4];
 
 // helper functions for lock and unlock
 
 void lock_quads(int q[], int n){
-    for(int i = 0; i < n; i++)
+    // sort the sections for lock:
+    for(int i = 0; i < n - 1; i++){
+        for(int j = i + 1; j < n; j++){
+            if(q[j] < q[i]){
+                int tmp = q[i];
+                q[i] = q[j];
+                q[j] = tmp;
+            }
+        }
+    }
+
+    // now locking the sections in the sorted order:
+    for(int i = 0; i < n; i++){
         pthread_mutex_lock(&sections[q[i]]);
+    }
 }
+
 
 void unlock_quads(int q[], int n){
     for(int i = 0; i < n; i++)
-        pthread_mutex_unlock(&sections[q[i]]);
+        pthread_mutex_unlock(&sections[q[i]]); 
 }
 
 
@@ -36,15 +60,27 @@ void turn_right(int id, int dir){
     int section_involved;
 
     switch(dir){
-        case NORTH: section_involved = 0; break;   
-        case EAST:  section_involved = 1; break;  
-        case SOUTH: section_involved = 2; break;  
-        case WEST:  section_involved = 3; break;   
+        case NORTH: 
+            section_involved = 0;
+            break;   
+        case EAST:  
+            section_involved = 1; 
+            break;  
+        case SOUTH: 
+            section_involved = 2; 
+            break;  
+        case WEST:  
+            section_involved = 3; 
+            break;   
     }
 
     pthread_mutex_lock(&sections[section_involved]);
 
-    printf("Car %d turning RIGHT from %d\n", id, dir);
+    printf("[ACTION] Car %d from %s turning RIGHT (section %d)\n",
+        id,
+        dir_to_string(dir),
+        section_involved);
+
     sleep(rand() % MAX_SLEEP);
 
     pthread_mutex_unlock(&sections[section_involved]);
@@ -56,22 +92,30 @@ void go_straight(int id, int dir){
 
     switch(dir){
         case NORTH:
-            sections_involoved[0] = 1; sections_involoved[1] = 2;
+            sections_involoved[0] = 1;
+            sections_involoved[1] = 2;
             break;
         case EAST:
-            sections_involoved[0] = 2; sections_involoved[1] = 3;
+            sections_involoved[0] = 2;
+            sections_involoved[1] = 3;
             break;
         case SOUTH:
-            sections_involoved[0] = 3; sections_involoved[1] = 0;
+            sections_involoved[0] = 3; 
+            sections_involoved[1] = 0;
             break;
         case WEST:
-            sections_involoved[0] = 0; sections_involoved[1] = 1;
+            sections_involoved[0] = 0; 
+            sections_involoved[1] = 1;
             break;
     }
 
     lock_quads(sections_involoved, 2);
 
-    printf("Car %d going STRAIGHT from %d\n", id, dir);
+    printf("[ACTION] Car %d from %s going STRAIGHT (sections %d -> %d)\n",
+        id,
+        dir_to_string(dir),
+        sections_involoved[0],
+        sections_involoved[1]);
     sleep(rand() % MAX_SLEEP);
 
     unlock_quads(sections_involoved, 2);
@@ -106,7 +150,12 @@ void turn_left(int id, int dir){
 
     lock_quads(sections_involoved, 3);
 
-    printf("Car %d turning LEFT from %d\n", id, dir);
+    printf("[ACTION] Car %d from %s turning LEFT (sections %d -> %d -> %d)\n",
+        id,
+        dir_to_string(dir),
+        sections_involoved[0],
+        sections_involoved[1],
+        sections_involoved[2]);
     sleep(rand() % MAX_SLEEP);
 
     unlock_quads(sections_involoved, 3);
@@ -126,6 +175,18 @@ void* car_spawner(void* args){
     for(int i = 0; i < count; i++){
         int move = rand() % 3;
 
+        int global_id = source * CAR_COUNT_PER_SPAWNER + i;
+        const char* move_name;
+        switch (move) {
+            case 0: move_name = "RIGHT";    break;
+            case 1: move_name = "STRAIGHT"; break;
+            case 2: move_name = "LEFT";     break;
+        }
+
+        printf("[INTENT] Car %d from %s wants to go %s\n",
+            global_id,
+            dir_to_string(source),
+            move_name);
         switch(move){
             case 0: turn_right(i, source);  break;
             case 1: go_straight(i, source); break;
